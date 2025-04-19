1a36e87bda1648fde7b7c02aa90823646f078de0
Filippo Petito <filippo.petito67@gmail.com>
2025-04-19 02:39:14 +0200

    First comment

---
+++ src/main/java/it/fpetito/jobvault/JobvaultApplication.java
@@ -0,0 +1,13 @@
+package it.fpetito.jobvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobvaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/fpetito/jobvault/JobvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package it.fpetito.jobvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
